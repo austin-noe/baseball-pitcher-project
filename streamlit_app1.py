0ece604447d3b55457a26fd4a374ae5c84d78592
austin-noe <117311142+austin-noe@users.noreply.github.com>
2022-12-11 18:25:48 -0500

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,46 +1,108 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import altair as alt
 import numpy as np
+st.set_page_config(layout="wide")
 
-
+st.header("Player Comparision Tool")
 @st.cache
 def get_data():
     path = r'Pitching.csv'
     return pd.read_csv(path)
 df = get_data()
+df2= get_data()
+tab1, tab2, tab3= st.tabs(["Player Comparision", "select player 1", "select player 2"])
+with tab2:
+    years = df['yearID'].drop_duplicates()
+    year_choice = st.selectbox('Please select a year', years) 
+    teams = df["TeamName"].loc[df["yearID"] == year_choice].drop_duplicates()
+    team_choice = st.selectbox('Please Select a team:', teams)
 
-years = df['yearID'].drop_duplicates()
-year_choice = st.sidebar.selectbox('select year', years) 
-teams = df["TeamName"].loc[df["yearID"] == year_choice].drop_duplicates()
-team_choice = st.sidebar.selectbox('Select your team:', teams)
-
-st.header(f" Pitching Stats for the {team_choice} in {year_choice} ")
-
-yeardf = df[(df.yearID == year_choice) & (df.IPouts >= 125)]
-
-yearrank = yeardf[["FullName","TeamName","ERA","R","W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO"]]
-yearrankavg = yearrank[["ERA","R","W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO"]].mean()
-stats = [" ","ERA","R","W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO"]
+    st.header(f" Pitching Stats for the {team_choice} in {year_choice} ")
 
 
 
-#placeholder
-PitcherType = [" ", "Starting", "Relief"]
-pitchtype = st.selectbox('Pitcher Type', PitcherType)
-if pitchtype == "Starting":
-    startdf = df[(df.yearID == year_choice) & (df.TeamName == team_choice) & (df.IPouts >= 125) & (df.GS > 0)] 
-    list = st.write(startdf[["FullName","ERA","R","W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO"]])
+    #placeholder
+    startdf = df[(df.yearID == year_choice) & (df.TeamName == team_choice) & (df.GS >= 1) ] 
     player = st.selectbox("Please Select a player",startdf['FullName'])
+    list = st.write(startdf[["FullName","ERA","R","W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO"]])
     teamrankavg = startdf[["ERA","R","W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO"]].mean()
-    playerstats = startdf[(df.FullName == player)]
-    playerstatsavg = playerstats[["ERA","R","W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO"]].mean()
-    st.write(playerstatsavg)
-  
+    player1stats = startdf[(df.FullName == player)]
+    playerstatsavg = player1stats[["ERA","R","W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO"]].mean()
+    st.write(player1stats)
+with tab3:
+    years2 = df2['yearID'].drop_duplicates()
+    year_choice2 = st.selectbox('Please select a year for player 2', years2) 
+    teams2 = df2["TeamName"].loc[df2["yearID"] == year_choice2].drop_duplicates()
+    team_choice2 = st.selectbox('Please Select a team for player 2:', teams2)
+
+    st.header(f" Pitching Stats for the {team_choice2} in {year_choice2} ")
+
+    year2df = df2[(df2.yearID == year_choice2)]
+    #placeholder
+    startdf2 = df2[(df2.yearID == year_choice2) & (df2.TeamName == team_choice2) & (df2.GS >= 1) ] 
+    player2 = st.selectbox("Please Select a 2nd player",startdf2['FullName'])
+    list2 = st.write(startdf2[["FullName","ERA","R","W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO"]])
+    teamrankavg = startdf2[["ERA","R","W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO"]].mean()
+    player2stats = startdf2[(df2.FullName == player2)]
+    playerstatsavg = player2stats[["ERA","R","W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO"]].mean()
+    st.write(player2stats)
+with tab1:
+    st.write("Please select a player on each of the tabs to generate the comparion below")
+    comparision = pd.concat([player1stats,player2stats])
+    st.header(f" You Have selected {year_choice} {player} and {year_choice2} {player2}")
     
-
-
-elif pitchtype == "Relief" :
-    reliefdf = df[(df.yearID == year_choice) & (df.TeamName == team_choice) & (df.IPouts >= 75) & (df.GS == 0)] 
-    list = st.write(reliefdf[["FullName","ERA","R","W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO"]])
-    player = st.selectbox("Please Select a player",reliefdf['FullName']) 
+    ChartERA = alt.Chart(comparision).mark_bar().encode(
+        x='FullName:N',
+        y='ERA:Q',
+        color='FullName:N',
+        )
+    ChartSO = alt.Chart(comparision).mark_bar().encode(
+        x='FullName:N',
+        y='SO:Q',
+        color='FullName:N',
+        )    
+    ChartW = alt.Chart(comparision).mark_bar().encode(
+        x='FullName:N',
+        y='W:Q',
+        color='FullName:N',
+        ) 
+    ChartL = alt.Chart(comparision).mark_bar().encode(
+        x='FullName:N',
+        y='L:Q',
+        color='FullName:N',
+        ) 
+    ChartG = alt.Chart(comparision).mark_bar().encode(
+        x='FullName:N',
+        y='G:Q',
+        color='FullName:N',
+        ) 
+    ChartH = alt.Chart(comparision).mark_bar().encode(
+        x='FullName:N',
+        y='H:Q',
+        color='FullName:N',
+        ) 
+    ChartER = alt.Chart(comparision).mark_bar().encode(
+        x='FullName:N',
+        y='ER:Q',
+        color='FullName:N',
+        ) 
+    ChartHR = alt.Chart(comparision).mark_bar().encode(
+        x='FullName:N',
+        y='HR:Q',
+        color='FullName:N',
+        ) 
+    ChartBB = alt.Chart(comparision).mark_bar().encode(
+        x='FullName:N',
+        y='BB:Q',
+        color='FullName:N',
+        ) 
+    ChartIPouts = alt.Chart(comparision).mark_bar().encode(
+        x='FullName:N',
+        y='IPouts:Q',
+        color='FullName:N',
+        ) 
+    charts = st.altair_chart(ChartERA | ChartSO | ChartW | ChartL | ChartG | ChartH | ChartBB | ChartHR |ChartER |ChartIPouts)    
+    st.write(charts)
